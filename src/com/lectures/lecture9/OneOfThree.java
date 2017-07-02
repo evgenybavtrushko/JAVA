5418f878f806aca7b5e15cf018328bcc596df5b5
evgenybavtrushko <evgeny.bavtrushko@gmail.com>
2017-07-02 18:54:12 +0300

    ??

--- src/com/lectures/lecture9/OneOfThree.java
+++ src/com/lectures/lecture9/OneOfThree.java
@@ -1,38 +1,28 @@
 package com.lectures.lecture9;
 
 import java.io.IOException;
-import java.io.InvalidObjectException;
-import java.io.ObjectStreamException;
-import java.text.ParseException;
-
 /**
  * Created by nimf8 on 02.07.2017.
  */
 public class OneOfThree {
     public static void main(String[] args) {
-        try{
-                oneOfThree();
-            }
-            catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-            catch (IOException e){
-               e.fillInStackTrace();
-        }
-        catch (IllegalAccessException e){
-                e.getMessage();
+        try {
+            oneOfThree();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | IllegalAccessException e) {
+            System.out.println("IOException или IllegalAccessException");
         }
 
     }
-    private static void oneOfThree() throws IllegalAccessException, IOException, ClassNotFoundException{
-        int x = (int) (Math.random()*15);
-        if( x < 4){
+
+    private static void oneOfThree() throws IllegalAccessException, IOException, ClassNotFoundException {
+        int x = (int) (Math.random() * 15);
+        if (x < 4) {
             throw new IllegalAccessException("dff");
-        }
-        else if (x < 8){
+        } else if (x < 8) {
             throw new IOException();
-        }
-        else{
+        } else {
             throw new ClassNotFoundException();
         }
     }
